5377d72b21a3333629aaf97a5304d7b0a0ef0097
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2023-11-27 13:41:45 +0100

    Avancement sur genea.h

--- genea.c
+++ genea.c
@@ -24,7 +24,49 @@ struct sFiche
 */
 tArbre ArbreCreer(void) {
     tArbre arbre = malloc(sizeof(struct sArbre));
-    arbre->pPremiere = malloc(sizeof(struct sFiche));
-    arbre->pDerniere = malloc(sizeof(struct sFiche));
+    arbre->pPremiere = NULL;
+    arbre->pDerniere = NULL;
     return arbre;
+}
+
+/*
+    Affiche à l’écran les informations contenues dans l’arbre
+    représenté par Arbre. Cette fonction doit afficher, pour chaque personne présente dans l’arbre,
+    les informations sous la forme des trois lignes suivantes :
+    identité de la personne
+    7−→Père : identité du père de la personne
+    7−→Mère : identité de la mère de la personne
+    où 7−→ représente le caractère tabulation (’\t’) et les identités sont affichées selon le format de
+    la fonction IdentiteAfficher du module identite. Si un parent n’est pas présent dans l’arbre,
+    c’est le mot inconnu qui doit être affiché à la place de son identité.
+    Voici le résultat de l’affichage de l’arbre généalogique constitué des quatre personnes citées
+    précédemment :
+    ```
+        [122] DUBOURG Pierre , M , 15/12/1910
+        P è re : [18] DUBOURG Jean , M , 21/07/1866
+        M è re : [19] COURBIN Marthe , F , 22/04/1872
+        [9] DUBOURG Catherine , F , 21/08/1904
+        P è re : [18] DUBOURG Jean , M , 21/07/1866
+        M è re : [19] COURBIN Marthe , F , 22/04/1872
+        [19] COURBIN Marthe , F , 22/04/1872
+        P è re : inconnu
+        M è re : inconnue
+        [18] DUBOURG Jean , M , 21/07/1866
+        P è re : inconnu
+        M è re : inconnue
+    ```
+*/
+void ArbreAfficher(tArbre Arbre) {
+    struct sFiche * fiche = Arbre->pPremiere;
+    //todo fiche == null
+    while (fiche->pSuivante != NULL) {
+        IdentiteAfficher(fiche->Identite);
+        printf("\tPère : ");
+        IdentiteAfficher(fiche->pPere->Identite);
+        printf("\n");
+        printf("\tMère : ");
+        IdentiteAfficher(fiche->pMere->Identite);
+
+        fiche = fiche->pSuivante;
+    } 
 }
--- makefile
+++ makefile
@@ -2,16 +2,18 @@ CC=gcc
 CFLAGS=-std=c11 -Wpedantic -Wall -Wextra
 LDLIBS=-lm
 RM=rm -f
-OBJ=main.o identite.o
+OBJ=main.o identite.o genea.o
 EXE=main
 # Pour produire le fichier exécutable
 $(EXE):$(OBJ)
 	$(CC) $(CFLAGS) $(OBJ) -o $(EXE) $(LDLIBS)
 # Pour produire les fichiers objets
-main.o:main.c identite.h
+main.o:main.c identite.h genea.h
 	$(CC) $(CFLAGS) -c main.c
 identite.o:identite.c identite.h
 	$(CC) $(CFLAGS) -c identite.c
+genea.o:genea.c genea.h
+	$(CC) $(CFLAGS) -c genea.c
 
 # Pour effacer les objets et les exécutables
 clean:
